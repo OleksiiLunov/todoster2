714ba04062321f18e52ba33aa40b7b6fb487aef3
OleksiiLunov <pridneprovskiyleshiy@gmail.com>
2026-05-19 20:33:43 +0300

    redirect authenticated users from auth pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,12 @@
-"use client";
-import AuthForm, { } from "@/features/auth/components/auth-form"
+import AuthForm from "@/features/auth/components/auth-form"
+import { getServerUser } from "@/lib/auth/server-user";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-
-    return (
-        <AuthForm mode="signup" />
-    );
+export default async function Page() {
+    if (!await getServerUser()) {
+        return (
+            <AuthForm mode="signup" />
+        );
+    }    
+    redirect("/");
 }
